a532aedbb0efedbc760d7eed7e3d050a411decf9
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-09-26 11:17:31 +0200

    csv-writer.cpp : Writing more info to nodes.csv Added a couple of edge types to edges.csv Using write-to-disk to save nodeID across visitor class instantiations <- strictly a HFN

--- clang-joern-frontend/src/exporters/csv-writer/csv-writer.cpp
+++ clang-joern-frontend/src/exporters/csv-writer/csv-writer.cpp
@@ -1,10 +1,15 @@
 #include "csv-writer.h"
 #include "clang/AST/Expr.h"
+#include "clang/AST/Decl.h"
 #include <sys/stat.h>
 
 using namespace clang;
 
 namespace exporter {
+
+using codePropTy	 = csvWriter::codePropTy;
+using nodeIDTy 		 = csvWriter::nodeIDTy;
+using fileTy 		 = csvWriter::fileTy;
 
 bool csvFilesExist() {
   struct stat buf;
@@ -16,11 +21,38 @@ bool csvFilesExist() {
   return false;
 }
 
+bool doesNodeIDFileExist() {
+  struct stat buf;
+  std::string nodeIDFilename = ".nodeID";
+  if (stat(nodeIDFilename.c_str(), &buf) != -1)
+    return true;
+  return false;
+}
+
+nodeIDTy readPrevNodeIDFromFile() {
+  std::fstream nodeFile;
+  nodeIDTy tmp;
+  nodeFile.open(".nodeID", std::ios::in);
+  nodeFile >> tmp;
+  // Delete file
+  if (remove(".nodeID") != 0)
+    llvm_unreachable("Error deleting nodeID file");
+  return tmp;
+}
+
+void writeNodeIDToTmpFile(nodeIDTy id) {
+  fileTy nodeIDFile;
+  nodeIDFile.open(".nodeID", std::ios::out);
+  nodeIDFile << id;
+  nodeIDFile.close();
+}
+
 void csvWriter::writeHeaders() {
   csvHeaderTy nodeHeader = {
       "nodeID:ID", "nodeKind", "loc", "locRange", \
       "type", "valueKind", "value", "castKind", \
-      "declName"
+      "declName", "semcontext", "lexcontext", \
+      "declqual"
   };
 
   csvHeaderTy edgeHeader = {
@@ -52,6 +84,7 @@ void csvWriter::init() {
   else {
     nodeFile.open("nodes.csv", std::ofstream::app);
     edgeFile.open("edges.csv", std::ofstream::app);
+    nodeID = readPrevNodeIDFromFile();
   }
 }
 
@@ -62,84 +95,134 @@ void csvWriter::flushBuffers() {
 
 void csvWriter::writeNodeRowWrapper() {
   if (!nodeRowMap.empty())
-    writeNodeRow(nodeRowMap, nodeFile);
+    writeRow(nodeRowMap, nodeFile, FIRST, LAST);
 }
 
-void csvWriter::writeNodeRow(csvRowTy &row, fileTy &file) {
-  for (int i = NODECOL::FIRST; i <= NODECOL::LAST; i++) {
+void csvWriter::writeEdgeRowWrapper() {
+  if (!edgeRowMap.empty())
+    writeRow(edgeRowMap, edgeFile, EFIRST, ELAST);
+}
+
+void csvWriter::writeRow(csvRowTy &row, fileTy &file, colIndexTy start, colIndexTy end) {
+  for (colIndexTy i = start; i <= end; i++) {
       auto column = row.find(i);
       if (column == row.end()) {
-	  if (i == NODECOL::LAST)
+	  if (i == end)
 	    file << "" << std::endl;
 	  else
 	    file << "" << "\t";
 	  continue;
       }
-      if (i == NODECOL::LAST)
+      if (i == end)
 	file << column->second << std::endl;
       else
 	file << column->second << "\t";
   }
 }
 
+void csvWriter::writeEdgeRow(codePropTy node1, codePropTy node2, codePropTy rel) {
+  edgeRowMap.emplace(ENODEID1, node1);
+  edgeRowMap.emplace(ENODEID2, node2);
+  edgeRowMap.emplace(ETYPE, rel);
+  writeRow(edgeRowMap, edgeFile, EFIRST, ELAST);
+  edgeRowMap.clear();
+}
+
 void csvWriter::exportDecl(const Decl *D) {
 
+  declNodeMap.emplace(D, ++nodeID);
   {
-  nodeRowMap.emplace(NODECOL::NODEID, std::to_string(++nodeID));
-  nodeRowMap.emplace(NODECOL::NODEKIND, D->getDeclKindName());
-  nodeRowMap.emplace(NODECOL::LOC, printLocation(D->getLocation()));
-  nodeRowMap.emplace(NODECOL::LOCRANGE, printSourceRange(D->getSourceRange()));
+    if (D->getLexicalDeclContext() != D->getDeclContext())
+      writeEdgeRow(std::to_string(nodeID),
+                   getNodeIDFromDeclPtr(cast<Decl>(D->getDeclContext())),
+                   EDGERELKEYS[SEMANTIC_PARENT]);
+
+    nodeRowMap.emplace(NODEID, std::to_string(nodeID));
+    nodeRowMap.emplace(NODEKIND, D->getDeclKindName());
+    // Defer filling location to TUD visit
+    if (!isa<TranslationUnitDecl>(D))
+      nodeRowMap.emplace(LOC, getLocation(D->getLocation()));
+    nodeRowMap.emplace(LOCRANGE, getSourceRange(D->getSourceRange()));
   }
+}
+
+void csvWriter::exportNamedDecl(const NamedDecl *ND) {
+    if (ND->getDeclName())
+      nodeRowMap.emplace(DECLNAME, ND->getNameAsString());
+}
+
+void csvWriter::exportTranslationUnitDecl(std::string filename) {
+  nodeRowMap.emplace(LOC, filename);
 }
 
 void csvWriter::exportStmt(const Stmt *S) {
 
+  stmtNodeMap.emplace(S, ++nodeID);
   {
-  nodeRowMap.emplace(NODECOL::NODEID, std::to_string(++nodeID));
-  nodeRowMap.emplace(NODECOL::NODEKIND, S->getStmtClassName());
-  nodeRowMap.emplace(NODECOL::LOC, "");
-  nodeRowMap.emplace(NODECOL::LOCRANGE, printSourceRange(S->getSourceRange()));
+    nodeRowMap.emplace(NODEID, std::to_string(nodeID));
+    nodeRowMap.emplace(NODEKIND, S->getStmtClassName());
+    nodeRowMap.emplace(LOC, "");
+    nodeRowMap.emplace(LOCRANGE, getSourceRange(S->getSourceRange()));
   }
 }
 
 void csvWriter::exportExpr(const Expr *E) {
+  nodeRowMap.emplace(TYPE, getType(E->getType()));
   {
     switch (E->getValueKind()) {
     case VK_RValue:
-      nodeRowMap.emplace(NODECOL::VALUEKIND, "");
+      nodeRowMap.emplace(VALUEKIND, "");
       break;
     case VK_LValue:
-      nodeRowMap.emplace(NODECOL::VALUEKIND, "lvalue");
+      nodeRowMap.emplace(VALUEKIND, "lvalue");
       break;
     case VK_XValue:
-      nodeRowMap.emplace(NODECOL::VALUEKIND, "xvalue");
+      nodeRowMap.emplace(VALUEKIND, "xvalue");
       break;
     }
   }
 }
 
+void csvWriter::exportCastExpr(const CastExpr *CE) {
+  nodeRowMap.emplace(CASTKIND, CE->getCastKindName());
+}
+
+void csvWriter::exportDeclRefExpr(const DeclRefExpr *DRE) {
+  codePropTy declNode = getNodeIDFromDeclPtr(cast<Decl>(DRE->getDecl()));
+
+  if (declNode.empty())
+    return;
+
+  edgeRowMap.emplace(ENODEID1, std::to_string(nodeID));
+  edgeRowMap.emplace(ENODEID2, declNode);
+  edgeRowMap.emplace(ETYPE, EDGERELKEYS[DECLREF_EXPR]);
+  writeRow(edgeRowMap, edgeFile, EFIRST, ELAST);
+  edgeRowMap.clear();
+}
+
 void csvWriter::closeFiles() {
   nodeFile.close();
   edgeFile.close();
+  writeNodeIDToTmpFile(nodeID);
 }
 
-std::string csvWriter::printSourceRange(SourceRange SR) {
+codePropTy csvWriter::getSourceRange(SourceRange SR) {
 
-  std::string sourceRange;
+  codePropTy sourceRange;
   llvm::raw_string_ostream OS(sourceRange);
 
   OS << " <";
-  OS << printLocation(SR.getBegin());
+  OS << getLocation(SR.getBegin());
   if (SR.getBegin() != SR.getEnd()) {
     OS << ", ";
-    OS << printLocation(SR.getEnd());
+    OS << getLocation(SR.getEnd());
   }
   OS << ">";
   return OS.str();
 }
 
-std::string csvWriter::printLocation(SourceLocation SL) {
-  std::string location;
+codePropTy csvWriter::getLocation(SourceLocation SL) {
+  codePropTy location;
   llvm::raw_string_ostream OS(location);
 
   SourceManager &SM = getASTContext()->getSourceManager();
@@ -169,6 +252,46 @@ std::string csvWriter::printLocation(SourceLocation SL) {
   }
 
   return OS.str();
+}
+
+// Obtain both sugared and optionally desugared types
+codePropTy csvWriter::getBareType(QualType T, bool Desugar) {
+  codePropTy typeinfo;
+  llvm::raw_string_ostream OS(typeinfo);
+
+  SplitQualType T_split = T.split();
+  OS << "<" << QualType::getAsString(T_split);
+
+  if (Desugar && !T.isNull()) {
+    // If the type is sugared, also dump a (shallow) desugared type.
+    SplitQualType D_split = T.getSplitDesugaredType();
+    if (T_split != D_split)
+      OS << ", " << QualType::getAsString(D_split) << ">";
+    else
+      OS << ">";
+  }
+  else {
+    OS << ">";
+  }
+  return OS.str();
+}
+
+codePropTy csvWriter::getType(QualType T) {
+  return getBareType(T);
+}
+
+codePropTy csvWriter::getNodeIDFromDeclPtr(const Decl *D) {
+  auto i = declNodeMap.find(D);
+  if (i != declNodeMap.end())
+    return std::to_string(i->second);
+  return codePropTy();
+}
+
+codePropTy csvWriter::getNodeIDFromStmtPtr(const Stmt *S) {
+  auto i = stmtNodeMap.find(S);
+  if (i != stmtNodeMap.end())
+    return std::to_string(i->second);
+  return codePropTy();
 }
 
 } // end of exporter namespace
