e99f2ce0bef0fcb561c978974ecaef60f77cb752
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-09-26 11:14:57 +0200

    IExporter.h : More exporter APIs

--- clang-joern-frontend/src/exporters/IExporter.h
+++ clang-joern-frontend/src/exporters/IExporter.h
@@ -9,8 +9,12 @@ class IExporter {
 public:
   IExporter(clang::ASTContext *ASTC) : Context(ASTC) {}
   virtual void exportDecl(const clang::Decl *D) = 0;
+  virtual void exportNamedDecl(const clang::NamedDecl *ND) = 0;
+  virtual void exportTranslationUnitDecl(std::string filename) = 0;
   virtual void exportStmt(const clang::Stmt *S) = 0;
   virtual void exportExpr(const clang::Expr *E) = 0;
+  virtual void exportCastExpr(const clang::CastExpr *CE) = 0;
+  virtual void exportDeclRefExpr(const clang::DeclRefExpr *DRE) = 0;
   virtual ~IExporter() {}
   clang::ASTContext *getASTContext() { return Context; }
 private:
